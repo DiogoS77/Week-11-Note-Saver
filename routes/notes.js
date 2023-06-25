e053afaf9b4daf38d5b55fcc3f15037bcd2ca251
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-25 03:47:09 +0100

    added route for new ux/ui , and adjusted uuid name

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Router } = require('express');
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 const notesRouter = Router();
@@ -32,15 +32,41 @@ notesRouter.get('/:note_id', async (req, res) => {
 });
 
 notesRouter.delete('/:note_id', async (req, res) => {
-    try {
-      const noteId = req.params.note_id;
-      const data = await readFromFile('./db/db.json');
-      const json = JSON.parse(data);
-      const result = json.filter((note) => note.id !== noteId);
-      await writeToFile('./db/db.json', result);
-      res.json(`Item ${noteId} got deleted`);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
+  try {
+    const noteId = req.params.note_id;
+    const data = await readFromFile('./db/db.json');
+    const json = JSON.parse(data);
+    const result = json.filter((note) => note.id !== noteId);
+    await writeToFile('./db/db.json', result);
+    res.json(`Item ${noteId} got deleted`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+notesRouter.post('/', async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+
+      await readAndAppend(newNote, './db/db.json');
+      res.json(`Note was added successfully`);
+    } else {
+      res.status(400).json('Error in adding this Note');
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = notesRouter;
